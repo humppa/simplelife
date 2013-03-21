91962abc1c655b9717e9b428b3071e35c6e26607
Tuomas Starck <dev@starck.fi>
2013-03-21 17:17:08 +0200

    Somewhat GUI

--- src/fi/starck/simplelife/gui/GUI.java
+++ src/fi/starck/simplelife/gui/GUI.java
@@ -1,6 +1,7 @@
 package fi.starck.simplelife.gui;
 
-import fi.starck.simplelife.LifeSet;
+import java.text.NumberFormat;
+import javax.swing.JFormattedTextField;
 import javax.swing.JPanel;
 
 /**
@@ -9,39 +10,35 @@ import javax.swing.JPanel;
 public class GUI extends JPanel {
     public GUI() {
         initComponents();
-        life = new LifeSet(32, 24);
-        life.glider();
     }
 
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        delaySlider = new javax.swing.JSlider();
-        stepSlider = new javax.swing.JSlider();
-        runButton = new javax.swing.JButton();
+        scrollBox = new javax.swing.JScrollPane();
         lifeCanvas = new fi.starck.simplelife.gui.LifeCanvas();
+        menuPanel = new javax.swing.JPanel();
+        labelSize = new javax.swing.JLabel();
+        formX = new JFormattedTextField(intFormat);
+        formY = new JFormattedTextField(intFormat);
+        labelCell = new javax.swing.JLabel();
+        formCell = new JFormattedTextField(intFormat);
+        labelDelay = new javax.swing.JLabel();
+        formDelay = new JFormattedTextField(floatFormat);
+        stepButton = new javax.swing.JButton();
+        walkButton = new javax.swing.JButton();
 
-        delaySlider.setMaximum(10000);
-        delaySlider.setMinimum(100);
-        delaySlider.setToolTipText("Time delay between steps");
-        delaySlider.setValue(1000);
+        setBorder(null);
+        setMinimumSize(new java.awt.Dimension(566, 454));
 
-        stepSlider.setMaximum(20);
-        stepSlider.setMinimum(1);
-        stepSlider.setToolTipText("Number of steps");
-        stepSlider.setValue(1);
+        scrollBox.setBorder(null);
 
-        runButton.setText("Run");
-        runButton.addMouseListener(new java.awt.event.MouseAdapter() {
-            public void mouseClicked(java.awt.event.MouseEvent evt) {
-                runButtonMouseClicked(evt);
-            }
-        });
-
+        lifeCanvas.setBorder(null);
+        lifeCanvas.setPreferredSize(new java.awt.Dimension(420, 360));
         lifeCanvas.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseClicked(java.awt.event.MouseEvent evt) {
-                lifeCanvasMouseClicked(evt);
+                mouseClick(evt);
             }
         });
 
@@ -49,11 +46,88 @@ public class GUI extends JPanel {
         lifeCanvas.setLayout(lifeCanvasLayout);
         lifeCanvasLayout.setHorizontalGroup(
             lifeCanvasLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 0, Short.MAX_VALUE)
+            .addGap(0, 564, Short.MAX_VALUE)
         );
         lifeCanvasLayout.setVerticalGroup(
             lifeCanvasLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 330, Short.MAX_VALUE)
+            .addGap(0, 404, Short.MAX_VALUE)
+        );
+
+        scrollBox.setViewportView(lifeCanvas);
+
+        labelSize.setText("Size of Game");
+
+        formX.setText("42");
+        formX.addPropertyChangeListener(new java.beans.PropertyChangeListener() {
+            public void propertyChange(java.beans.PropertyChangeEvent evt) {
+                formattedChange(evt);
+            }
+        });
+
+        formY.setText("36");
+        formY.addPropertyChangeListener(new java.beans.PropertyChangeListener() {
+            public void propertyChange(java.beans.PropertyChangeEvent evt) {
+                formattedChange(evt);
+            }
+        });
+
+        labelCell.setText("Cell size");
+
+        formCell.setText("10");
+        formCell.addPropertyChangeListener(new java.beans.PropertyChangeListener() {
+            public void propertyChange(java.beans.PropertyChangeEvent evt) {
+                formattedChange(evt);
+            }
+        });
+
+        labelDelay.setText("Delay");
+
+        formDelay.setText("1");
+        formDelay.addPropertyChangeListener(new java.beans.PropertyChangeListener() {
+            public void propertyChange(java.beans.PropertyChangeEvent evt) {
+                formattedChange(evt);
+            }
+        });
+
+        stepButton.setText("Step");
+
+        walkButton.setText("Walk");
+
+        javax.swing.GroupLayout menuPanelLayout = new javax.swing.GroupLayout(menuPanel);
+        menuPanel.setLayout(menuPanelLayout);
+        menuPanelLayout.setHorizontalGroup(
+            menuPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(menuPanelLayout.createSequentialGroup()
+                .addComponent(labelSize)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(formX, javax.swing.GroupLayout.PREFERRED_SIZE, 42, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(formY, javax.swing.GroupLayout.PREFERRED_SIZE, 42, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(labelCell)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(formCell, javax.swing.GroupLayout.PREFERRED_SIZE, 42, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(labelDelay)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(formDelay, javax.swing.GroupLayout.PREFERRED_SIZE, 42, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(stepButton)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(walkButton))
+        );
+        menuPanelLayout.setVerticalGroup(
+            menuPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(menuPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                .addComponent(labelSize)
+                .addComponent(formX, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(formY, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(labelCell)
+                .addComponent(formCell, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(labelDelay)
+                .addComponent(formDelay, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(stepButton)
+                .addComponent(walkButton))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
@@ -62,45 +136,71 @@ public class GUI extends JPanel {
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(lifeCanvas, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addGroup(layout.createSequentialGroup()
-                        .addComponent(delaySlider, javax.swing.GroupLayout.DEFAULT_SIZE, 223, Short.MAX_VALUE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                        .addComponent(runButton, javax.swing.GroupLayout.PREFERRED_SIZE, 66, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                        .addComponent(stepSlider, javax.swing.GroupLayout.DEFAULT_SIZE, 223, Short.MAX_VALUE)))
-                .addContainerGap())
+                .addComponent(menuPanel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+            .addComponent(scrollBox)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(stepSlider, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(delaySlider, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(runButton, javax.swing.GroupLayout.Alignment.TRAILING))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(lifeCanvas, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addContainerGap())
+                .addComponent(menuPanel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(scrollBox, javax.swing.GroupLayout.DEFAULT_SIZE, 406, Short.MAX_VALUE))
         );
     }// </editor-fold>//GEN-END:initComponents
 
-    private void runButtonMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_runButtonMouseClicked
-        life.step();
-        System.out.println(life.toString());
-    }//GEN-LAST:event_runButtonMouseClicked
+    /**
+     * Receive and handle change events in formatted text fields.
+     *
+     * NPE is ignored, because in some situations JFormattedTextField.getValue()
+     * returns null. It is ignored silently, because the value does not interest
+     * us in those situations.
+     *
+     * @param evt Event.
+     */
+    private void formattedChange(java.beans.PropertyChangeEvent evt) {//GEN-FIRST:event_formattedChange
+        try {
+            if (evt.getSource() == formX) {
+                lifeCanvas.setWidth(((Number) formX.getValue()).intValue());
+            }
+            else if (evt.getSource() == formY) {
+                lifeCanvas.setHeight(((Number) formY.getValue()).intValue());
+            }
+            else if (evt.getSource() == formCell) {
+                lifeCanvas.setUnit(((Number) formCell.getValue()).intValue());
+            }
+            else if (evt.getSource() == formDelay) {
+                lifeCanvas.setDelay(((Number) formDelay.getValue()).floatValue());
+            }
+        }
+        catch (NullPointerException ignored) {}
+    }//GEN-LAST:event_formattedChange
 
-    private void lifeCanvasMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_lifeCanvasMouseClicked
-        // TODO add your handling code here:
-        lifeCanvas.tr(evt.getX(), evt.getY());
-    }//GEN-LAST:event_lifeCanvasMouseClicked
+    /**
+     * Pass mouse clicks to canvas.
+     *
+     * @param evt Event.
+     */
+    private void mouseClick(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_mouseClick
+        lifeCanvas.flipBit(evt.getX(), evt.getY());
+    }//GEN-LAST:event_mouseClick
 
-    private LifeSet life;
+    private final NumberFormat intFormat = NumberFormat.getIntegerInstance();
+    private final NumberFormat floatFormat = NumberFormat.getNumberInstance();
+
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JSlider delaySlider;
+    private javax.swing.JFormattedTextField formCell;
+    private javax.swing.JFormattedTextField formDelay;
+    private javax.swing.JFormattedTextField formX;
+    private javax.swing.JFormattedTextField formY;
+    private javax.swing.JLabel labelCell;
+    private javax.swing.JLabel labelDelay;
+    private javax.swing.JLabel labelSize;
     private fi.starck.simplelife.gui.LifeCanvas lifeCanvas;
-    private javax.swing.JButton runButton;
-    private javax.swing.JSlider stepSlider;
+    private javax.swing.JPanel menuPanel;
+    private javax.swing.JScrollPane scrollBox;
+    private javax.swing.JButton stepButton;
+    private javax.swing.JButton walkButton;
     // End of variables declaration//GEN-END:variables
 }
--- src/fi/starck/simplelife/gui/LifeCanvas.java
+++ src/fi/starck/simplelife/gui/LifeCanvas.java
@@ -1,5 +1,6 @@
 package fi.starck.simplelife.gui;
 
+import fi.starck.simplelife.LifeSet;
 import java.awt.Graphics;
 import javax.swing.JPanel;
 
@@ -8,14 +9,69 @@ import javax.swing.JPanel;
  */
 public class LifeCanvas extends JPanel {
     public LifeCanvas() {
+        width = 42;
+        height = 36;
+        unit = 10;
+        delay = 1;
+        createNewLife();
     }
 
-    void tr(int x, int y) {
-        System.out.println("Kysytään koordinaatteja " + x + "," + y);
+    void setWidth(int w) {
+        if (w <3) return;
+        width = w;
+        createNewLife();
+    }
+
+    void setHeight(int h) {
+        if (h <3) return;
+        height = h;
+        createNewLife();
+    }
+
+    void setUnit(int u) {
+        unit = u;
+        repaint();
+    }
+
+    void setDelay(float d) {
+        delay = d;
+    }
+
+    void flipBit(int x, int y) {
+        life.flipLife(y/unit, x/unit);
+        repaint();
     }
 
     @Override
     public void paintComponent(Graphics g) {
-        System.out.println("paintComponent yrittää piirrellä");
+        super.paintComponent(g);
+
+        for (int i=life.nextSetBit(0); i>=0; i=life.nextSetBit(i+1)) {
+            int x = i%width;
+            int y = i/width;
+
+            System.out.println("u::" + unit + " i::" + i + " => " + x + "," + y + " (w,h) (" + width + "," + height + ")");
+
+            g.fillRect(x*unit, y*unit, unit, unit);
+        }
+
+        System.out.println();
+    }
+
+    private int unit;
+    private int width;
+    private int height;
+    private float delay;
+    private LifeSet life;
+
+    private void createNewLife() {
+        life = new LifeSet(width, height);
+
+        System.out.println("new life by " + width + "," + height);
+        System.out.println(life.toString());
+
+        repaint();
+
+        // life.glider(); // // DEBUG // //
     }
 }
